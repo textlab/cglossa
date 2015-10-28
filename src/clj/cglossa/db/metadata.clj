e2a94643c8f8114103a6181dd817f168018a4f27
Anders Nøklestad <anders.noklestad@iln.uio.no>
2015-10-28 17:31:32 +0100

    WIP on putting several constraints on metadata

--- src/clj/cglossa/db/metadata.clj
+++ src/clj/cglossa/db/metadata.clj
@@ -1,5 +1,6 @@
 (ns cglossa.db.metadata
-  (:require [cglossa.db.shared :refer [sql-query]]))
+  (:require [cglossa.db.shared :refer [build-sql sql-query]]
+            [clojure.string :as str]))
 
 (def ^:private metadata-pagesize 100)
 
@@ -16,23 +17,42 @@
     [total res]))
 
 (defn constrained-metadata-values [selected-ids category-id skip limit]
+  #_(str "SELECT @rid AS id, value AS text FROM MetadataValue "
+         "WHERE @rid IN (SELECT intersect($1, $2) "
+         "LET $1 = "
+         "(SELECT EXPAND(out('DescribesText').in('DescribesText')"
+         "[corpus_cat = 'bokmal_translation']) "
+         "FROM #14:9438),"
+         "$2 = "
+         "(SELECT EXPAND(out('DescribesText').in('DescribesText')"
+         "[corpus_cat = 'bokmal_translation']) "
+         "FROM #14:16810))")
   (let [corpus-cat    (-> (sql-query "SELECT corpus_cat FROM #TARGET" {:target category-id})
                           first
                           :corpus_cat)
         initial-value (first (vals selected-ids))
+        fragments     (for [targets (vals selected-ids)]
+                        (build-sql (str "(SELECT EXPAND(out('DescribesText').in('DescribesText')"
+                                        "[corpus_cat = '&category']) "
+                                        "FROM #TARGETS)")
+                                   {:targets targets
+                                    :strings {:category corpus-cat}}))
+        res-sql       (str "SELECT @rid AS id, value AS text FROM MetadataValue WHERE @rid IN "
+                           "(SELECT intersect("
+                           (str/join ", " (map (fn [i] (str \$ i)) (range (count fragments))))
+                           ") LET "
+                           (str/join ", " (map-indexed (fn [i frag]
+                                                         (str \$ i " = " frag))
+                                                       fragments))
+                           ") ORDER BY value SKIP &skip LIMIT &limit")
+        _ (println res-sql)
         total         (-> (sql-query (str "SELECT out('DescribesText').in('DescribesText')"
                                           "[corpus_cat = '&category'].size() AS total FROM #TARGET")
                                      {:target  initial-value
                                       :strings {:category corpus-cat}})
                           first
                           :total)
-        res           (sql-query
-                        (str "SELECT @rid AS id, value AS text FROM "
-                             "(SELECT EXPAND(out('DescribesText').in('DescribesText')"
-                             "[corpus_cat = '&category']) "
-                             "FROM #TARGET ORDER BY value SKIP &skip LIMIT &limit)")
-                        {:target  initial-value
-                         :strings {:category corpus-cat :skip skip :limit limit}})]
+        res           (sql-query res-sql {:strings {:skip skip :limit limit}})]
     [total res]))
 
 (defn get-metadata-values [category-id selected-ids page]
