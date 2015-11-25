b6b4284ce1d0a36560a0d48a47cf7cf35fa9b63a
Anders Nøklestad <anders.noklestad@iln.uio.no>
2015-11-25 15:36:37 +0100

    Skip unnecessary join with metadata_categories

--- src/cljs/cglossa/show_texts.cljs
+++ src/cljs/cglossa/show_texts.cljs
@@ -17,7 +17,7 @@
                                                :page              page}}))
             body     (:body response)]
         (if (http/unexceptional-status? (:status response))
-          (let [cat-names (sort (map :name @metadata-categories))
+          (let [cat-names (map :name (sort-by :id @metadata-categories))
                 rows      (map zipmap (repeat cat-names) body)]
             (reset! results rows))
           (.error js/console (str "Error: " body))))))
@@ -48,32 +48,32 @@
        #(get-data 1)
 
        :reagent-render
-       (fn [_ _]
+       (fn [_ {:keys [metadata-categories]}]
          [b/modal {:bs-size "large"
                    :show    true
                    :on-hide hide}
           [b/modalheader {:close-button true}
            [b/modaltitle "Corpus texts"]]
           [b/modalbody
-           [table {:columns                 (map :name @metadata-categories)
-                   :use-external            true
-                   :external-set-page       #()
-                   :enable-sort             false
-                   :external-set-page-size  #()
-                   :external-max-page       @max-pages
-                   :external-change-sort    #()
-                   :external-set-filter     #()
-                   :external-current-page   @current-page
-                   :results                 @results
-                   :table-class-name        "table"
-                   :results-per-page        @external-results-per-page
-                   :external-sort-column    @external-sort-column
-                   :external-sort-ascending @external-sort-ascending
+           [table {:columns                    (map :name (sort-by :id @metadata-categories))
+                   :use-external               true
+                   :external-set-page          #()
+                   :enable-sort                false
+                   :external-set-page-size     #()
+                   :external-max-page          @max-pages
+                   :external-change-sort       #()
+                   :external-set-filter        #()
+                   :external-current-page      @current-page
+                   :results                    @results
+                   :table-class-name           "table"
+                   :results-per-page           @external-results-per-page
+                   :external-sort-column       @external-sort-column
+                   :external-sort-ascending    @external-sort-ascending
                    :external-loading-component loading-comp
-                   :external-is-loading     @loading?
-                   :enable-infinite-scroll  true
-                   :body-height             (- (.. (ViewportSizeMonitor.) getSize -height) 300)
-                   :body-width              1000
-                   :use-fixed-header        true}]]
+                   :external-is-loading        @loading?
+                   :enable-infinite-scroll     true
+                   :body-height                (- (.. (ViewportSizeMonitor.) getSize -height) 300)
+                   :body-width                 1000
+                   :use-fixed-header           true}]]
           [b/modalfooter
            [b/button {:on-click hide} "Close"]]])})))
