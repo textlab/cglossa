7f9d58a260dc89bf3396f4dec2a95fd27d95a81b
Anders Nøklestad <anders.noklestad@iln.uio.no>
2015-12-03 00:50:05 +0100

    Add more languages to UD

--- src/cljs/cglossa/search_views/ud/core.cljs
+++ src/cljs/cglossa/search_views/ud/core.cljs
@@ -4,31 +4,68 @@
             [cglossa.shared :refer [search! on-key-down]]
             [cglossa.search-views.shared :refer [search-inputs]]))
 
-(def languages ["English" "Norwegian"])
-(def selected-language (r/atom "Norwegian"))
+(def languages ["English"
+                "Ancient_Greek"
+                "Ancient_Greek-PROIEL"
+                "Arabic"
+                "Basque"
+                "Bulgarian"
+                "Croatian"
+                "Czech"
+                "Danish"
+                "Dutch"
+                "Estonian"
+                "Finnish"
+                "Finnish-FTB"
+                "French"
+                "German"
+                "Gothic"
+                "Greek"
+                "Hebrew"
+                "Hindi"
+                "Hungarian"
+                "Indonesian"
+                "Irish"
+                "Italian"
+                "Japanese-KTC"
+                "Latin"
+                "Latin-ITT"
+                "Latin-PROIEL"
+                "Norwegian"
+                "Old_Church_Slavonic"
+                "Persian"
+                "Polish"
+                "Portuguese"
+                "Romanian"
+                "Slovenian"
+                "Spanish"
+                "Swedish"
+                "Tamil"
+                "Fi-Parsebank-50M"
+                "Fi-Parsebank-1M"])
 
 (defmethod search-inputs "ud" [{{:keys [queries]} :search-view :as a} m]
-  (let [query (first @queries)]
-    [:span
-     [:div.row.search-input-links
-      [:div.col-sm-3
-       [b/input {:type      "select"
-                 :style     {:width 150}
-                 :on-change #(swap! queries assoc-in [0 :lang] (.-target.value %))}
-        (for [language languages]
-          [:option {:key language :value language} language])]]
-      [:div.col-sm-9
-       [b/button {:bs-style "success"
-                  :style    {:margin-left 233}
-                  :on-click #(search! a m)} "Search"]]]
-     [:div.row
-      [:div.col-sm-12
-       [:form.table-display {:style {:margin "10px 0px 15px 0px"}}
-        [:div.table-row {:style {:margin-bottom 10}}
-         [b/input {:style            {:width 522}
-                   :class-name       "col-sm-12"
-                   :group-class-name "table-cell"
-                   :type             "textarea"
-                   :default-value    (:query (first @queries))
-                   :on-change        #(swap! queries assoc-in [0 :query] (.-target.value %))
-                   :on-key-down      #(on-key-down % a m)}]]]]]]))
+  [:span
+   [:div.row.search-input-links
+    [:div.col-sm-3
+     [b/input {:type          "select"
+               :style         {:width 150}
+               :default-value (:lang (first @queries))
+               :on-change     #(swap! queries assoc-in [0 :lang] (.-target.value %))}
+      (for [language languages]
+        [:option {:key language :value language} language])]]
+    [:div.col-sm-9
+     [b/button {:bs-style "success"
+                :style    {:margin-left 233}
+                :on-click #(search! a m)} "Search"]]]
+   [:div.row
+    [:div.col-sm-12
+     [:form.table-display {:style {:margin "10px 0px 15px 0px"}}
+      [:div.table-row {:style {:margin-bottom 10}}
+       [b/input {:style            {:width 522}
+                 :class-name       "col-sm-12"
+                 :group-class-name "table-cell"
+                 :type             "textarea"
+                 :default-value    (:query (first @queries))
+                 :on-change        #(swap! queries assoc-in [0 :query] (.-target.value %))
+                 :on-key-down      #(on-key-down % a m)}]]]]]])
