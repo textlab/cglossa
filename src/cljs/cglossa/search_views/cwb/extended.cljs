379d8bfd588a71baa10db71a1c2922a4c9e39677
Anders Nøklestad <anders.noklestad@iln.uio.no>
2016-01-26 19:13:55 +0100

    Only show grammar options for langs that have them

--- src/cljs/cglossa/search_views/cwb/extended.cljs
+++ src/cljs/cglossa/search_views/cwb/extended.cljs
@@ -151,15 +151,15 @@
 (defn- menu-button [{{:keys [show-attr-popup-for]} :search-view :as a} {:keys [menu-data] :as m}
                     wrapped-query wrapped-term index]
   (let [selected-language (:lang @wrapped-query)
-        menu-data* (get @menu-data selected-language)]
+        menu-data*        (get @menu-data selected-language)]
     (list
       ^{:key "btn"}
-      [b/dropdown {:id (str "search-term-pos-dropdown-" index)
+      [b/dropdown {:id    (str "search-term-pos-dropdown-" index)
                    :style {:width 59}}
        [b/button {:bs-size  "small"
                   :on-click #(reset! show-attr-popup-for index)}
         [b/glyphicon {:glyph "list"}]]
-       [b/dropdown-toggle {:bs-size "small"}]
+       [b/dropdown-toggle {:bs-size "small" :disabled (nil? menu-data*)}]
        [b/dropdown-menu
         (for [[pos title] menu-data*
               :let [selected? (contains? (:features @wrapped-term) pos)]]
@@ -178,46 +178,52 @@
                 :show       (= @show-attr-popup-for index)
                 :on-hide    #(hide-attr-popup show-attr-popup-for)}
        [b/modalbody
-        [b/panel {:header "Parts-of-speech"}
-         (doall (for [[pos title] menu-data*
-                      :let [selected? (contains? (:features @wrapped-term) pos)]]
-                  ^{:key pos}
-                  [b/button
-                   {:style    {:margin-left 3 :margin-top 2 :margin-bottom 3}
-                    :bs-size  "xsmall"
-                    :bs-style (if selected? "info" "default")
-                    :on-click (fn [_] (swap! wrapped-term update :features
-                                             #(if selected? (dissoc % pos) (assoc % pos {}))))}
-                   title]))]
-        (for [[pos title morphsyn] menu-data*
-              :when (and (contains? (:features @wrapped-term) pos)
-                         (seq morphsyn))]
-          ^{:key pos}
-          [b/panel {:header (str "Morphosyntactic features for " title)}
-           [:div.table-display
-            (for [[header attrs] (partition 2 morphsyn)]
-              ^{:key header}
-              [:div.table-row
-               [:div.table-cell header ": "]
-               [:div.table-cell {:style {:padding-bottom 5}}
-                (doall (for [[attr value title] attrs
-                             :let [attr*     (name attr)
-                                   selected? (contains? (get-in @wrapped-term [:features pos attr*])
-                                                        value)]]
-                         ^{:key value}
-                         [b/button {:style    {:margin-left 3 :margin-bottom 5}
-                                    :bs-size  "xsmall"
-                                    :bs-style (if selected? "info" "default")
-                                    :on-click (fn [_]
-                                                (swap! wrapped-term update-in [:features pos attr*]
-                                                       (fn [a] (if selected?
-                                                                 (disj a value)
-                                                                 (set (conj a value)))))
-                                                (if (empty? (get-in @wrapped-term
-                                                                    [:features pos attr*]))
-                                                  (swap! wrapped-term update-in [:features pos]
-                                                         dissoc attr*)))}
-                          title]))]])]])]
+        (when menu-data*
+          (list
+            ^{:key "pos-panel"}
+            [b/panel {:header "Parts-of-speech"}
+             (doall (for [[pos title] menu-data*
+                          :let [selected? (contains? (:features @wrapped-term) pos)]]
+                      ^{:key pos}
+                      [b/button
+                       {:style    {:margin-left 3 :margin-top 2 :margin-bottom 3}
+                        :bs-size  "xsmall"
+                        :bs-style (if selected? "info" "default")
+                        :on-click (fn [_] (swap! wrapped-term update :features
+                                                 #(if selected? (dissoc % pos) (assoc % pos {}))))}
+                       title]))]
+            ^{:key "pos"}
+            (for [[pos title morphsyn] menu-data*
+                  :when (and (contains? (:features @wrapped-term) pos)
+                             (seq morphsyn))]
+              ^{:key pos}
+              [b/panel {:header (str "Morphosyntactic features for " title)}
+               [:div.table-display
+                (for [[header attrs] (partition 2 morphsyn)]
+                  ^{:key header}
+                  [:div.table-row
+                   [:div.table-cell header ": "]
+                   [:div.table-cell {:style {:padding-bottom 5}}
+                    (doall (for [[attr value title] attrs
+                                 :let [attr*     (name attr)
+                                       selected? (contains? (get-in @wrapped-term
+                                                                    [:features pos attr*])
+                                                            value)]]
+                             ^{:key value}
+                             [b/button {:style    {:margin-left 3 :margin-bottom 5}
+                                        :bs-size  "xsmall"
+                                        :bs-style (if selected? "info" "default")
+                                        :on-click (fn [_]
+                                                    (swap! wrapped-term
+                                                           update-in [:features pos attr*]
+                                                           (fn [a] (if selected?
+                                                                     (disj a value)
+                                                                     (set (conj a value)))))
+                                                    (if (empty? (get-in @wrapped-term
+                                                                        [:features pos attr*]))
+                                                      (swap! wrapped-term update-in [:features pos]
+                                                             dissoc attr*)))}
+                              title]))]])]])))]
        [b/modalfooter
         [b/button {:bs-style "danger"
                    :on-click #(swap! wrapped-term assoc :features nil)} "Clear"]
@@ -234,7 +240,7 @@
                              :width     108}
              :button-before (r/as-element (menu-button a m wrapped-query wrapped-term index))
              :button-after  (when show-remove-term-btn?
-                              (r/as-element [b/button {:title "Remove search word"
+                              (r/as-element [b/button {:title    "Remove search word"
                                                        :on-click #(reset! wrapped-term nil)}
                                              [b/glyphicon {:glyph "minus"}]]))
              :default-value (if (:form @wrapped-term)
