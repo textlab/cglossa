fd74bf7af45e81a817caea2b291aac44c0d13a3e
Anders Nøklestad <anders.noklestad@iln.uio.no>
2016-01-26 14:11:38 +0100

    Refactor routes into a separate namespace

--- src/clj/cglossa/server.clj
+++ src/clj/cglossa/server.clj
@@ -1,36 +1,22 @@
 (ns cglossa.server
-  (:require [clojure.java.io :as io]
-            [clojure.walk :as walk]
-            [clojure.string :as str]
-            [compojure.core :refer [GET POST defroutes routes context]]
-            [compojure.route :refer [resources]]
-            [net.cgrand.enlive-html :refer [deftemplate html-content]]
-            [ring.util.response :as response]
+  (:require [compojure.core :refer [routes]]
             [ring.middleware.reload :as reload]
             [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [ring.middleware.json :refer [wrap-json-params]]
-            [ring.handler.dump :refer [handle-dump]]
-            [prone.middleware :refer [wrap-exceptions]]
             [environ.core :refer [env]]
             [org.httpkit.server :refer [run-server]]
             [clojure.tools.logging :as log]
-            [cognitect.transit :as transit]
-            [cheshire.core :as cheshire]
             [korma.db :as kdb]
-            [korma.core :refer [defentity select fields belongs-to]]
-            [cglossa.shared :refer [core-db]]
-            [cglossa.db.corpus :refer [corpus corpus-by-code]]
-            [cglossa.db.metadata :refer [get-metadata-categories get-metadata-values show-texts]]
-            [cglossa.search.core :as search]
+            [korma.core :refer [select fields]]
+            [cglossa.shared :refer [corpus-connections core-db]]
+            [cglossa.routes :refer [app-routes db-routes search-routes]]
+            [cglossa.db.corpus :refer [corpus]]
             [cglossa.search-engines])
-  (:import [java.io ByteArrayOutputStream])
   (:gen-class))
 
-(defonce corpus-connections (atom {}))
-
-(defn- init-corpus-connections! []
-  (reset! corpus-connections
+(defn- init-corpus-connections! [connections]
+  (reset! connections
           (into {} (for [c (select corpus (fields :id :code))]
                      [(:id c)
                       (kdb/create-db (kdb/mysql {:user     (:glossa-db-user env)
@@ -47,42 +33,6 @@
       (uncaughtException [_ thread ex]
         (log/error ex "Uncaught exception on" (.getName thread))))))
 
-(defn- hyphenize-keys
-  "Recursively changes underscore to hyphen in all map keys, which are assumed
-   to be strings or keywords."
-  [m]
-  (let [f (fn [[k v]] [(keyword (str/replace (name k) "_" "-")) v])]
-    (walk/postwalk #(if (map? %) (into {} (map f %)) %) m)))
-
-(defn- transit-response*
-  ([body]
-   (transit-response* body true))
-  ([body hyphenize?]
-   (let [baos   (ByteArrayOutputStream. 2000)
-         writer (transit/writer baos (if (:is-dev env) :json-verbose :json))
-         _      (transit/write writer (if hyphenize? (hyphenize-keys body) body))
-         res    (.toString baos)]
-     (.reset baos)
-     (-> (response/response res)
-         (response/content-type "application/transit+json")
-         (response/charset "utf-8")))))
-
-(defmacro transit-response [fn-call & args]
-  `(try (let [res# ~fn-call]
-          (transit-response* res# ~@args))
-        (catch Exception e#
-          (log/error e#)
-          {:status 500
-           :body   (.toString e#)})))
-
-(deftemplate page (io/resource "index.html") [])
-(deftemplate admin (io/resource "admin.html")
-             []
-             [:#corpus-table]
-             (html-content
-               (str "<tr><td style=\"width: 350px;\">AA</td>"
-                    "<td><button class=\"btn btn-danger btn-xs\">Delete</td></tr>")))
-
 (defn wrap-db
   "Middleware that checks if the request contains a corpus-id key, and if so,
   sets the database for the given corpus as the default for the request. Otherwise
@@ -97,64 +47,9 @@
                       core-db)]
       (kdb/with-db db (handler request)))))
 
-(defroutes app-routes
-  (resources "/")
-  (GET "/request" [] handle-dump)
-  (GET "/" req (page))
-  (GET "/admin" req (admin)))
-
-;;;;;;;;;;;;
-;; Routes
-;;;;;;;;;;;;
-
-(defroutes db-routes
-  (GET "/corpus" [code]
-    (if-let [c (corpus-by-code code)]
-      (let [cats    (kdb/with-db (get @corpus-connections (:id c)) (get-metadata-categories))
-            tagfile (case (:code c)
-                      "gigaword_fre_3" "treetagger_fr"
-                      "obt_bm_lbk")
-            [gram-titles & menu-data] (read-string (slurp (str "resources/taggers/"
-                                                               tagfile ".edn")))]
-        (transit-response {:corpus              c
-                           :metadata-categories cats
-                           :gram-titles         gram-titles
-                           :menu-data           menu-data}))
-      {:status 500
-       :body   (str "No corpus named " code " exists!")}))
-
-  (POST "/corpus" [zipfile]
-    (println zipfile))
-
-  (GET "/metadata-values" [category-id value-filter selected-ids page]
-    (let [selected-ids* (when selected-ids (cheshire/parse-string selected-ids))
-          page*         (if page (Integer/parseInt page) 1)
-          data          (get-metadata-values category-id value-filter selected-ids* page*)]
-      (-> (response/response (cheshire/generate-string {:results    (:results data)
-                                                        :pagination {:more (:more? data)}}))
-          (response/content-type "application/json")
-          (response/charset "utf-8"))))
-
-  (POST "/texts" [selected-metadata ncats page]
-    (let [ncats* (or ncats 1)
-          page*  (or page 1)]
-      (transit-response (show-texts selected-metadata ncats* page*)))))
-
-(defroutes search-routes
-  (POST "/search" [corpus-id search-id queries metadata-ids step cut sort-key]
-    (transit-response (search/search-corpus corpus-id search-id queries metadata-ids
-                                            step cut sort-key) false))
-
-  (GET "/results" [corpus-id search-id start end sort-key]
-    (transit-response (search/results corpus-id search-id start end sort-key) false)))
-
-;;;;;;;;;;;;;;
-;; End routes
-;;;;;;;;;;;;;;
-
 (def http-handler
   (let [r (routes #'db-routes #'search-routes #'app-routes)
-        r (if (:is-dev env) (-> r reload/wrap-reload wrap-exceptions) r)]
+        r (if (:is-dev env) (-> r reload/wrap-reload) r)]
     (-> r
         wrap-db
         wrap-keyword-params
@@ -163,7 +58,7 @@
 
 (defn run [& [port]]
   (set-default-exception-handler!)
-  (init-corpus-connections!)
+  (init-corpus-connections! corpus-connections)
   (defonce ^:private server
     (do
       (let [port (Integer. (or port (env :port) 10555))]
--- src/clj/cglossa/shared.clj
+++ src/clj/cglossa/shared.clj
@@ -2,6 +2,8 @@
   (:require [environ.core :refer [env]]
             [korma.db :as kdb]))
 
+(defonce corpus-connections (atom {}))
+
 (def core-db-name (str (get env :glossa-prefix "glossa") "__core"))
 
 (kdb/defdb core-db (kdb/mysql {:user     (:glossa-db-user env)
