bca121e769c3224be5cce64587cfeed86712363c
Tekstlab <tekstlab-post@iln.uio.no>
2016-06-05 18:15:10 +0200

    Fix id column for text corpora

--- src/cljs/cglossa/result_views/cwb/written.cljs
+++ src/cljs/cglossa/result_views/cwb/written.cljs
@@ -60,7 +60,7 @@
 (defn- main-row [result index a {:keys [corpus] :as m}]
   ^{:key (hash result)}
   [:tr
-   (shared/id-column a m result)
+   (shared/id-column a m result index)
    (shared/text-columns result)])
 
 (defn single-result-rows [a m res index]
