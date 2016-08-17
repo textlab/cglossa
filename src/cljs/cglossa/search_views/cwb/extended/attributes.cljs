d218885ea837269e62d986dfedde0b181ff638d6
Anders Nøklestad <anders.noklestad@iln.uio.no>
2016-08-17 13:40:08 +0200

    Formatting

--- src/cljs/cglossa/search_views/cwb/extended/attributes.cljs
+++ src/cljs/cglossa/search_views/cwb/extended/attributes.cljs
@@ -121,53 +121,53 @@
 (defn menu-button [a {:keys [corpus] :as m}
                    wrapped-query wrapped-term index show-attr-popup?]
   (r/with-let [options-clicked (atom false)]
-              (let [selected-language (:lang @wrapped-query)
-                    menu-data         (language-menu-data corpus selected-language)]
-                (list
-                  ^{:key "btn"}
-                  [b/dropdown {:id    (str "search-term-pos-dropdown-" index)
-                               :style {:width 59}}
-                   [b/button {:bs-size  "small"
-                              :on-click #(reset! show-attr-popup? true)}
-                    [b/glyphicon {:glyph "list"}]]
-                   [b/dropdown-toggle {:bs-size "small" :disabled (nil? menu-data)}]
-                   [b/dropdown-menu {:style {:min-width 180}}
-                    (for [[pos title tooltip] menu-data
-                          :let [selected? (contains? (:features @wrapped-term) pos)]]
-                      ^{:key pos}
-                      [b/menuitem {:active      selected?
-                                   :data-toggle (when tooltip "tooltip")
-                                   :title       tooltip
-                                   :on-click    (fn [_]
-                                                  (if selected?
-                                                    ;; Deselect the part-of-speech if it was
-                                                    ;; selected AND we didn't click the options
-                                                    ;; icon (because in that case we want to
-                                                    ;; specify morphosyntactic features, not
-                                                    ;; deselect).
-                                                    (when-not @options-clicked
-                                                      (swap! wrapped-term update :features
-                                                             dissoc pos))
-                                                    ;; If it was not selected, we select it
-                                                    ;; regardless of whether or not the options
-                                                    ;; icon was clicked.
-                                                    (swap! wrapped-term assoc-in [:features pos]
-                                                           {}))
-                                                  (reset! options-clicked false))}
-                       (or title pos) [b/glyphicon
-                                       {:glyph    "option-horizontal"
-                                        :title    "More options"
-                                        :style    {:float "right" :margin-left 5 :margin-top 3}
-                                        :on-click (fn [e]
-                                                    ;; Clicking the option icon on a menu item both
-                                                    ;; selects the part-of-speech and opens the
-                                                    ;; popup for advanced attributes
-                                                    (.preventDefault e)
-                                                    ;; Set a flag to make sure we don't deselect
-                                                    ;; this part-of-speech when the event bubbles
-                                                    ;; up to the menu item if it was already
-                                                    ;; selected
-                                                    (reset! options-clicked true)
-                                                    (reset! show-attr-popup? true))}]])]]
-                  ^{:key "modal"}
-                  [attribute-modal a m wrapped-query wrapped-term menu-data show-attr-popup?]))))
+    (let [selected-language (:lang @wrapped-query)
+          menu-data         (language-menu-data corpus selected-language)]
+      (list
+        ^{:key "btn"}
+        [b/dropdown {:id    (str "search-term-pos-dropdown-" index)
+                     :style {:width 59}}
+         [b/button {:bs-size  "small"
+                    :on-click #(reset! show-attr-popup? true)}
+          [b/glyphicon {:glyph "list"}]]
+         [b/dropdown-toggle {:bs-size "small" :disabled (nil? menu-data)}]
+         [b/dropdown-menu {:style {:min-width 180}}
+          (for [[pos title tooltip] menu-data
+                :let [selected? (contains? (:features @wrapped-term) pos)]]
+            ^{:key pos}
+            [b/menuitem {:active      selected?
+                         :data-toggle (when tooltip "tooltip")
+                         :title       tooltip
+                         :on-click    (fn [_]
+                                        (if selected?
+                                          ;; Deselect the part-of-speech if it was
+                                          ;; selected AND we didn't click the options
+                                          ;; icon (because in that case we want to
+                                          ;; specify morphosyntactic features, not
+                                          ;; deselect).
+                                          (when-not @options-clicked
+                                            (swap! wrapped-term update :features
+                                                   dissoc pos))
+                                          ;; If it was not selected, we select it
+                                          ;; regardless of whether or not the options
+                                          ;; icon was clicked.
+                                          (swap! wrapped-term assoc-in [:features pos]
+                                                 {}))
+                                        (reset! options-clicked false))}
+             (or title pos) [b/glyphicon
+                             {:glyph    "option-horizontal"
+                              :title    "More options"
+                              :style    {:float "right" :margin-left 5 :margin-top 3}
+                              :on-click (fn [e]
+                                          ;; Clicking the option icon on a menu item both
+                                          ;; selects the part-of-speech and opens the
+                                          ;; popup for advanced attributes
+                                          (.preventDefault e)
+                                          ;; Set a flag to make sure we don't deselect
+                                          ;; this part-of-speech when the event bubbles
+                                          ;; up to the menu item if it was already
+                                          ;; selected
+                                          (reset! options-clicked true)
+                                          (reset! show-attr-popup? true))}]])]]
+        ^{:key "modal"}
+        [attribute-modal a m wrapped-query wrapped-term menu-data show-attr-popup?]))))
