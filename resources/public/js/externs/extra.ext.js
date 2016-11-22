8ee9e269209c9ba23e119a991ae7d993c9700ea0
Anders Nøklestad <anders.noklestad@iln.uio.no>
2016-11-22 02:14:33 +0100

    Missing extern

--- resources/public/js/externs/extra.ext.js
+++ resources/public/js/externs/extra.ext.js
@@ -2,6 +2,7 @@ var ReactBootstrap = {};
 ReactBootstrap.Dropdown = {};
 ReactBootstrap.Dropdown.Menu = {};
 ReactBootstrap.Dropdown.Toggle = {};
+ReactBootstrap.Navbar = {};
 ReactBootstrap.Navbar.Brand = {};
 ReactBootstrap.Navbar.Link = {};
 ReactBootstrap.Navbar.Text = {};
