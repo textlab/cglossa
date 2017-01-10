c9bff420be7715ef81d8e0a3caa09e7cd1200830
Anders Nøklestad <anders.noklestad@iln.uio.no>
2017-01-11 00:20:33 +0100

    Fix call to module.hot.accept

--- src/fable/Source/Entry.fsx
+++ src/fable/Source/Entry.fsx
@@ -19,8 +19,8 @@ let [<Global>] [<Emit("module")>] Module : IModule = failwith "JS only"
 
 let node = document.querySelector ("." + contentNodeClass)
 
-  Module.hot?accept() |> ignore
 if not <| isNull Module.hot then
+  Module.hot?accept("./Main.js") |> ignore
 
   Module.hot?dispose(fun _ ->
     node.removeChild(node.firstChild) |> ignore
