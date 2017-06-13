1f5a603597f649007947abb2261f4070ad850c80
Michał Kosek <mihao@users.noreply.github.com>
2017-06-13 21:07:18 +0200

    tmp/.keep gets confusing when tmp is a symlink. Use startup scripts to make sure tmp exists.

--- start_dev.sh
+++ start_dev.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 . ./config.sh
+mkdir -p tmp
 lein run >>log/dev.log &
 rlwrap lein figwheel
 pkill -f " -Dleiningen.original.pwd=`pwd` .* run$"
--- start_prod.sh
+++ start_prod.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 . ./config.sh
+mkdir -p tmp
 java -jar "`pwd -P`/target/cglossa.jar" >>log/prod.log 2>&1 &
