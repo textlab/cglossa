e5f17ffba3ef66231cc40af4e004b2a9500f98ab
Michał Kosek <mihao@users.noreply.github.com>
2017-06-14 04:42:30 +0200

    Link to the homepage

--- src/cljs/cglossa/app.cljs
+++ src/cljs/cglossa/app.cljs
@@ -19,7 +19,8 @@
 
 (defn- header [{:keys [show-results?]} {:keys [corpus authenticated-user]}]
   [b/navbar {:fixed-top true}
-   [b/navbar-brand "Glossa"]
+   [b/navbar-brand
+     [:a {:href "./"} "Glossa"]]
    (when @show-results?
      ;; Only show corpus name in the header when showing results, since
      ;; it is shown in big letters on the front page
