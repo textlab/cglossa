65b8491b1e518d8d07636b7d7025baebddce5ee7
Anders Nøklestad <anders.noklestad@iln.uio.no>
2017-09-11 12:45:57 +0200

    Add corpus-specific code for CANS

---
+++ src/cljs/cglossa/corpora/cans.cljs
@@ -0,0 +1,28 @@
+(ns cglossa.corpora.cans
+  (:require [clojure.string :as str]
+            [cglossa.react-adapters.bootstrap :as b]
+            [cglossa.results :refer [result-links]]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
+
+(defmethod result-links "cans" [{{:keys [page-no translations]} :results-view :as a}
+                                _ result row-index]
+  (let [translation-key (str @page-no "_" row-index)
+        on-click        (fn [e]
+                          (go
+                            (let [key      "PRIVATE"
+                                  url      "https://www.googleapis.com/language/translate/v2"
+                                  text     (str/join " " (:full-text result))
+                                  response (<! (http/get url {:with-credentials? false
+                                                              :query-params      {:key    key
+                                                                                  :target "en"
+                                                                                  :q      text}}))
+                                  trans    (get-in response [:body :data :translations 0 :translatedText])]
+                              (swap! translations assoc translation-key trans))))]
+    [:div {:style {:margin-top 5}}
+     [b/button {:bs-size  "xsmall"
+                :style    {:color "#666" :font-size 10}
+                :on-click on-click
+                :disabled (not (nil? (get @translations translation-key)))}
+      "Translate"]]))
--- src/cljs/cglossa/corpora/core.cljs
+++ src/cljs/cglossa/corpora/core.cljs
@@ -2,6 +2,7 @@
   "Require namespaces for corpus-specific code here."
   (:require cglossa.corpora.elenor
             cglossa.corpora.amerikanorsk
+            cglossa.corpora.cans
             cglossa.corpora.scandiasyn
             cglossa.corpora.norint-tekst
             cglossa.corpora.norint-opplest
