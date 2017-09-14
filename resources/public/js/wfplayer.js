5249b0ac9cd9bad7e4970e3932ba22ddb664aa01
Anders Nøklestad <anders.noklestad@iln.uio.no>
2017-09-14 12:11:59 +0200

    Send movieLoc instead of lineKey to wave

--- resources/public/js/wfplayer.js
+++ resources/public/js/wfplayer.js
@@ -12,7 +12,7 @@
       start = mediaObj.divs.annotation[this.props.startAt || parseInt(mediaObj.startAt)].from;
       stop = mediaObj.divs.annotation[this.props.endAt || parseInt(mediaObj.endAt)].to;
       corpusCode = mediaObj.mov.path.match(/.+\/(.+)/)[1];
-      return $node.find("#waveframe").attr('src', "https://tekstlab.uio.no/wave/wfplayer-" + corpusId + "-" + lineKey + "-" + start + "-" + stop);
+      return $node.find("#waveframe").attr('src', "https://tekstlab.uio.no/wave/wfplayer-" + corpusId + "-" + mediaObj.mov.movieLoc + "-" + start + "-" + stop);
     },
 
     render: function() {
