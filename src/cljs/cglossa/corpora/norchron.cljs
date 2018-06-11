d2b492754f692550eed0ad998382745f6423e775
Anders Nøklestad <anders.noklestad@iln.uio.no>
2018-06-11 15:19:51 +0200

    Show norchron doc in popup

--- src/cljs/cglossa/corpora/norchron.cljs
+++ src/cljs/cglossa/corpora/norchron.cljs
@@ -8,27 +8,34 @@
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (def ^:private show-document (r/atom nil))
+(def ^:private row-index-shown (r/atom nil))
 
 (defn- document-popup []
-  (let [hide    #(reset! show-document nil)
-        text-id (re-find #"^[^\.]+" @show-document)
-        {:keys [body]} (go (<! (http/get (str "/norchron/docs/"
-                                              text-id
-                                              ".xml.html"))))]
+  (r/with-let [hide (fn []
+                      (reset! show-document nil)
+                      (reset! row-index-shown nil))
+               text-id (re-find #"^[^\.]+" @show-document)
+               modal-body (r/atom nil)]
+    (go (let [{:keys [body]} (<! (http/get (str "/norchron/data/"
+                                                text-id
+                                                ".xml.html")))]
+          (reset! modal-body body)))
     [:div.show-norchron-document-popup
      [b/modal {:show              true
                :on-hide           hide
-               :dialog-class-name "show-texts-popup"}
+               :dialog-class-name "show-norchron-document-popup"}
       [b/modalheader {:close-button true}
        [b/modaltitle (str "Document " text-id)]]
-      [b/modalbody [:div "hei"]]
+      [b/modalbody [:div {:style {:padding 25 :position "relative"}
+                          :dangerouslySetInnerHTML {:__html @modal-body}}]]
       [b/modalfooter
        [b/button {:on-click hide} "Close"]]]]))
 
-(defmethod result-links "norchron" [_ _ result _]
+(defmethod result-links "norchron" [_ _ result row-index]
   (let [s-id     (:s-id result)
         on-click (fn [e]
                    (reset! show-document s-id)
+                   (reset! row-index-shown row-index)
                    ;; Need to manually remove the button tooltip; otherwise it
                    ;; may persist after the document opens in a different tab/window
                    ;; (and then there is no way to remove it). We also need to blur
@@ -40,7 +47,7 @@
                        (.blur)
                        (.tooltip "destroy")))]
     [:span
-     (when (= @show-document s-id)
+     (when (= @row-index-shown row-index)
        (document-popup))
      [:div {:style {:margin-top 5 :text-align "center"}}
       [b/button {:bs-size  "xsmall" :data-toggle "tooltip"
